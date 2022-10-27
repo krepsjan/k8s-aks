e6f6535fa9841bfe26f81d33c6ab19011e318fd9
Jan Kreps <jan@kreps.cz>
2022-10-27 12:33:54 +0200

    Update az-aks-get-credentials.sh

--- dev/k9s-aks/az-aks-get-credentials.sh
+++ dev/k9s-aks/az-aks-get-credentials.sh
@@ -3,5 +3,6 @@ LOCATION='eastus'
 AKS_NAME='aks210200'
 
 az aks get-credentials -g $RESOURCE_GROUP -n $AKS_NAME
-
+# az aks get-credentials --resource-group az_cluster --name my_aks_cluster
+# stahne konfig a prida ho do current konfigu v .kube/config
 az aks install-cli
